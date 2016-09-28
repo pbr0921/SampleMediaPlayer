024f22b30ec0e63a5e2acd3cbab6dc14dd514529
pbr0921 <pbr0921@naver.com>
2016-09-28 15:41:13 +0900

    test

--- app/src/main/java/com/example/boram/samplemediaplayer/intro/IntroActivity.java
+++ app/src/main/java/com/example/boram/samplemediaplayer/intro/IntroActivity.java
@@ -13,7 +13,6 @@ public class IntroActivity extends Activity {
 
     public Handler handler;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
