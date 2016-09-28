d4a918971a2eb925ed28fad617dfe9cf5e46e77c
UNIWIS <UNIWIS>
2016-09-28 14:14:09 +0900

    Initial commit

---
+++ app/src/main/java/com/example/boram/samplemediaplayer/Main/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.boram.samplemediaplayer.Main;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.example.boram.samplemediaplayer.R;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/example/boram/samplemediaplayer/intro/IntroActivity.java
@@ -0,0 +1,46 @@
+package com.example.boram.samplemediaplayer.intro;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+
+import com.example.boram.samplemediaplayer.Main.MainActivity;
+import com.example.boram.samplemediaplayer.R;
+
+public class IntroActivity extends Activity {
+
+    public Handler handler;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_intro);
+
+        handler = new Handler();
+        handler.postDelayed(runnable,3000);
+
+    }
+    Runnable runnable = new Runnable() {
+        @Override
+        public void run() {
+            Intent intent = new Intent(IntroActivity.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+            overridePendingTransition(android.R.anim.fade_in,android.R.anim.fade_out);
+
+        }
+    };
+
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        handler.removeCallbacks(runnable);
+    }
+}
+
+
